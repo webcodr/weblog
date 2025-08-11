0da61f5a008cf7b238eed19b759d10b3c7f9ac3b
David Henning <david@webcodr.io>
2025-08-11 20:04:38 +0200

    feat: update theme

--- assets/js/webcodr.js
+++ assets/js/webcodr.js
@@ -1,1 +1,51 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const i={golang:"go",sh:"bash"},d=r=>{var o;return i[o=r.toLowerCase()]??(i[o]=r)},a=()=>{const r=document.querySelectorAll("[data-lang]");if(r.length!==0)for(const o of r){const n=document.createElement("span"),s=o.getAttribute("data-lang"),e=document.createTextNode(d(s));n.appendChild(e),n.classList.add("post-content--language"),o.parentNode.prepend(n)}};document.addEventListener("DOMContentLoaded",()=>{a()});
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) return;
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) processPreload(link);
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") continue;
+      for (const node of mutation.addedNodes) if (node.tagName === "LINK" && node.rel === "modulepreload") processPreload(node);
+    }
+  }).observe(document, {
+    childList: true,
+    subtree: true
+  });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity) fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials") fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous") fetchOpts.credentials = "omit";
+    else fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep) return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+const languageAliases = {
+  golang: "go",
+  sh: "bash"
+};
+const sanitizeLanguageName = (language) => languageAliases[language.toLowerCase()] ??= language;
+const setLanguage = () => {
+  const codeBlocks = document.querySelectorAll("[data-lang]");
+  if (codeBlocks.length === 0) {
+    return;
+  }
+  for (const codeBlock of codeBlocks) {
+    const nameElement = document.createElement("span");
+    const language = codeBlock.getAttribute("data-lang");
+    const textNode = document.createTextNode(sanitizeLanguageName(language));
+    nameElement.appendChild(textNode);
+    nameElement.classList.add("post-content--language");
+    codeBlock.parentNode.prepend(nameElement);
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  setLanguage();
+});
