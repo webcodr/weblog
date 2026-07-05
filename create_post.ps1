5605ede6d2732a204da887d2c08eb3c53487845a
David Henning <david@webcodr.io>
2026-07-05 19:05:32 +0200

    fix: remove double dashes from slug

--- create_post.ps1
+++ create_post.ps1
@@ -6,7 +6,7 @@ function Create-Post {
     
     $date = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss+00:00")
     $fileDate = Get-Date -Format "yyyy-MM-dd"
-    $slug = $Title -replace '[^A-Za-z0-9]', '-' | ForEach-Object { $_.ToLower() }
+    $slug = $Title -replace '[^A-Za-z0-9]', '-' -replace '-+', '-' -replace '^-|-$', '' | ForEach-Object { $_.ToLower() }
     $filename = "content/post/${fileDate}_${slug}.md"
     
     $content = @"
